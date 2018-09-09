1ea82cab4ebc4f2896f6c1133d4f392b04ecf35c
Arkadiusz Okas <arkadiuszokas@gmail.com>
2018-09-09 19:11:20 +0200

    Ban list with doctrine

--- src/Controller/BansController.php
+++ src/Controller/BansController.php
@@ -5,23 +5,42 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Ban;
+use Doctrine\ORM\EntityManagerInterface;
 
 class BansController extends AbstractController
 {
 	/**
-	 * @Route("/bans")
+	 * @Route("/bans",name="ban_list")
 	 */
-	public function showBansContent()
+	public function showBansContent(EntityManagerInterface $em)
 	{
-		$bans = [
-			'arek1',
-			'player2',
-			'player3'
-		];
+		$repository = $em->getRepository(Ban::class);
+		$bans = $repository->findAll();
 
-		//return new Response('Lista banów tutaj!');
+		if(!$bans)
+			throw $this->createNotFoundException(sprintf('No bans'));
+
+
 		return $this->render('bans.html.twig',[
 			'bans'=>$bans
 		]);
 	}
+
+	/**
+	 * @Route("/bans/add/",name="bans_add",methods={"POST"})
+	 */
+	public function addBan(EntityManagerInterface $em)
+	{
+		$ban = new Ban();
+		$ban->setName('player_'.rand(1,100));
+		$ban->setAddedBy('Arek');
+		$em->persist($ban);
+		$em->flush();
+		//$this->bans[] = $name;
+		return $this->json(['bans'=>['id'=>$ban->getId(),'name'=>$ban->getName()]]);
+	}
+
 }
---
+++ src/Entity/Ban.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BanRepository")
+ */
+class Ban
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $addedBy;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddedBy(): ?string
+    {
+        return $this->addedBy;
+    }
+
+    public function setAddedBy(string $addedBy): self
+    {
+        $this->addedBy = $addedBy;
+
+        return $this;
+    }
+}
